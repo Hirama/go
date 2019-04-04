e3879c3c08288854350c686aeba6a913b3dee624
hirama <crrrr@mail.ru>
2019-04-04 16:06:27 +0300

    secp256k1

--- src/crypto/elliptic/elliptic.go
+++ src/crypto/elliptic/elliptic.go
@@ -53,211 +53,231 @@ func (curve *CurveParams) Params() *CurveParams {
 }
 
 func (curve *CurveParams) IsOnCurve(x, y *big.Int) bool {
-	// y² = x³ - 3x + b
+	// y^2
 	y2 := new(big.Int).Mul(y, y)
 	y2.Mod(y2, curve.P)
 
+	// x^3 + b
 	x3 := new(big.Int).Mul(x, x)
 	x3.Mul(x3, x)
-
-	threeX := new(big.Int).Lsh(x, 1)
-	threeX.Add(threeX, x)
-
-	x3.Sub(x3, threeX)
 	x3.Add(x3, curve.B)
 	x3.Mod(x3, curve.P)
 
-	return x3.Cmp(y2) == 0
+	return y2.Cmp(x3) == 0
 }
 
-// zForAffine returns a Jacobian Z value for the affine point (x, y). If x and
-// y are zero, it assumes that they represent the point at infinity because (0,
-// 0) is not on the any of the curves handled here.
-func zForAffine(x, y *big.Int) *big.Int {
-	z := new(big.Int)
-	if x.Sign() != 0 || y.Sign() != 0 {
-		z.SetInt64(1)
+func zForAffine(xA, yA *big.Int) (z *big.Int) {
+	z = new(big.Int)
+	if xA.Sign() != 0 || yA.Sign() != 0 {
+		return z.SetInt64(1)
 	}
-	return z
+	return // point at infinity
 }
 
-// affineFromJacobian reverses the Jacobian transform. See the comment at the
-// top of the file. If the point is ∞ it returns 0, 0.
-func (curve *CurveParams) affineFromJacobian(x, y, z *big.Int) (xOut, yOut *big.Int) {
+func (curve *CurveParams) affineFromJacobian(x, y, z *big.Int) (xA, yA *big.Int) {
+	// point at infinity
 	if z.Sign() == 0 {
 		return new(big.Int), new(big.Int)
 	}
 
-	zinv := new(big.Int).ModInverse(z, curve.P)
-	zinvsq := new(big.Int).Mul(zinv, zinv)
+	// 1/Z^1
+	zInv := new(big.Int).ModInverse(z, curve.P)
 
-	xOut = new(big.Int).Mul(x, zinvsq)
-	xOut.Mod(xOut, curve.P)
-	zinvsq.Mul(zinvsq, zinv)
-	yOut = new(big.Int).Mul(y, zinvsq)
-	yOut.Mod(yOut, curve.P)
+	// 1/Z^2
+	zzInv := new(big.Int).Mul(zInv, zInv)
+
+	// 1/Z^3
+	zzzInv := new(big.Int).Mul(zzInv, zInv)
+
+	// x = X/Z^2
+	xA = new(big.Int).Mul(x, zzInv)
+	xA.Mod(xA, curve.P)
+
+	// y = Y/Z^3
+	yA = new(big.Int).Mul(y, zzzInv)
+	yA.Mod(yA, curve.P)
+
 	return
 }
 
-func (curve *CurveParams) Add(x1, y1, x2, y2 *big.Int) (*big.Int, *big.Int) {
+func (curve *CurveParams) Add(x1, y1, x2, y2 *big.Int) (x, y *big.Int) {
 	z1 := zForAffine(x1, y1)
 	z2 := zForAffine(x2, y2)
 	return curve.affineFromJacobian(curve.addJacobian(x1, y1, z1, x2, y2, z2))
 }
 
-// addJacobian takes two points in Jacobian coordinates, (x1, y1, z1) and
-// (x2, y2, z2) and returns their sum, also in Jacobian form.
-func (curve *CurveParams) addJacobian(x1, y1, z1, x2, y2, z2 *big.Int) (*big.Int, *big.Int, *big.Int) {
-	// See https://hyperelliptic.org/EFD/g1p/auto-shortw-jacobian-3.html#addition-add-2007-bl
-	x3, y3, z3 := new(big.Int), new(big.Int), new(big.Int)
+// ref. https://hyperelliptic.org/EFD/g1p/auto-shortw-jacobian-3.html#addition-add-2007-bl
+func (curve *CurveParams) addJacobian(x1, y1, z1, x2, y2, z2 *big.Int) (x3, y3, z3 *big.Int) {
+	x3, y3, z3 = new(big.Int), new(big.Int), new(big.Int)
+
+	// (x1, y1) is point at infinity in affine coordinates
 	if z1.Sign() == 0 {
+		// (x3, y3) = (x2, y2) in affine coordinates
 		x3.Set(x2)
 		y3.Set(y2)
 		z3.Set(z2)
-		return x3, y3, z3
+		return
 	}
+
+	// (x2, y2) is point at infinity in affine coordinates
 	if z2.Sign() == 0 {
+		// (x3, y3) = (x1, y1) in affine coordinates
 		x3.Set(x1)
 		y3.Set(y1)
 		z3.Set(z1)
-		return x3, y3, z3
+		return
 	}
 
-	z1z1 := new(big.Int).Mul(z1, z1)
-	z1z1.Mod(z1z1, curve.P)
-	z2z2 := new(big.Int).Mul(z2, z2)
-	z2z2.Mod(z2z2, curve.P)
+	// Z1Z1 = Z1^2
+	z1z1 := new(big.Int).Mul(z1, z1) // 1S
 
-	u1 := new(big.Int).Mul(x1, z2z2)
+	// Z2Z2 = Z2^2
+	z2z2 := new(big.Int).Mul(z2, z2) // 2S
+
+	// U1 = X1*Z2Z2
+	u1 := new(big.Int).Mul(x1, z2z2) // 1M
 	u1.Mod(u1, curve.P)
-	u2 := new(big.Int).Mul(x2, z1z1)
-	u2.Mod(u2, curve.P)
-	h := new(big.Int).Sub(u2, u1)
-	xEqual := h.Sign() == 0
-	if h.Sign() == -1 {
-		h.Add(h, curve.P)
-	}
-	i := new(big.Int).Lsh(h, 1)
-	i.Mul(i, i)
-	j := new(big.Int).Mul(h, i)
 
-	s1 := new(big.Int).Mul(y1, z2)
-	s1.Mul(s1, z2z2)
+	// U2 = X2*Z1Z1
+	u2 := new(big.Int).Mul(x2, z1z1) // 2M
+	u2.Mod(u2, curve.P)
+
+	// S1 = Y1*Z2*Z2Z2
+	s1 := new(big.Int).Mul(y1, z2) // 3M
+	s1.Mul(s1, z2z2)               // 4M
 	s1.Mod(s1, curve.P)
-	s2 := new(big.Int).Mul(y2, z1)
-	s2.Mul(s2, z1z1)
+
+	// S2 = Y2*Z1*Z1Z1
+	s2 := new(big.Int).Mul(y2, z1) // 5M
+	s2.Mul(s2, z1z1)               // 6M
 	s2.Mod(s2, curve.P)
-	r := new(big.Int).Sub(s2, s1)
-	if r.Sign() == -1 {
-		r.Add(r, curve.P)
-	}
-	yEqual := r.Sign() == 0
-	if xEqual && yEqual {
+
+	// x1 == x2 and y1 == y2 in affine coordinates
+	if u1.Cmp(u2) == 0 && s1.Cmp(s2) == 0 {
 		return curve.doubleJacobian(x1, y1, z1)
 	}
-	r.Lsh(r, 1)
-	v := new(big.Int).Mul(u1, i)
 
-	x3.Set(r)
-	x3.Mul(x3, x3)
-	x3.Sub(x3, j)
-	x3.Sub(x3, v)
-	x3.Sub(x3, v)
+	// H = U2 - U1
+	h := new(big.Int).Sub(u2, u1) // 1add
+
+	// I = (2*H)^2
+	i := new(big.Int).Lsh(h, 1) // 1*2
+	i.Mul(i, i)                 // 3S
+
+	// J = H*I
+	j := new(big.Int).Mul(h, i) // 7M
+
+	// r = 2*(S2 - S1)
+	r := new(big.Int).Sub(s2, s1) // 2add
+	r.Lsh(r, 1)                   // 2*2
+
+	// V = U1*I
+	v := new(big.Int).Mul(u1, i) // 8M
+
+	// tmp1 = 2*V
+	tmp1 := new(big.Int).Lsh(v, 1) // 3*2
+
+	// tmp2 = 2*S1*J
+	tmp2 := new(big.Int).Mul(s1, j) // 9M
+	tmp2.Lsh(tmp2, 1)               // 4*2
+
+	// X3 = r^2 - J - 2*V
+	x3.Mul(r, r)     // 4S
+	x3.Sub(x3, j)    // 3add
+	x3.Sub(x3, tmp1) // 4add
 	x3.Mod(x3, curve.P)
 
-	y3.Set(r)
-	v.Sub(v, x3)
-	y3.Mul(y3, v)
-	s1.Mul(s1, j)
-	s1.Lsh(s1, 1)
-	y3.Sub(y3, s1)
+	// Y3 = r*(V - X3) - 2*S1*J
+	y3.Sub(v, x3)    // 5add
+	y3.Mul(r, y3)    // 10M
+	y3.Sub(y3, tmp2) // 6add
 	y3.Mod(y3, curve.P)
 
-	z3.Add(z1, z2)
-	z3.Mul(z3, z3)
-	z3.Sub(z3, z1z1)
-	z3.Sub(z3, z2z2)
-	z3.Mul(z3, h)
+	// Z3 = ((Z1 + Z2)^2 - Z1Z1 - Z2Z2)*H
+	z3.Add(z1, z2)   // 7add
+	z3.Mul(z3, z3)   // 5S
+	z3.Sub(z3, z1z1) // 8add
+	z3.Sub(z3, z2z2) // 9add
+	z3.Mul(z3, h)    // 11M
 	z3.Mod(z3, curve.P)
 
-	return x3, y3, z3
+	// cost: 11M + 5S + 9add + 4*2
+
+	return
 }
 
-func (curve *CurveParams) Double(x1, y1 *big.Int) (*big.Int, *big.Int) {
+func (curve *CurveParams) Double(x1, y1 *big.Int) (x, y *big.Int) {
 	z1 := zForAffine(x1, y1)
 	return curve.affineFromJacobian(curve.doubleJacobian(x1, y1, z1))
 }
 
-// doubleJacobian takes a point in Jacobian coordinates, (x, y, z), and
-// returns its double, also in Jacobian form.
-func (curve *CurveParams) doubleJacobian(x, y, z *big.Int) (*big.Int, *big.Int, *big.Int) {
-	// See https://hyperelliptic.org/EFD/g1p/auto-shortw-jacobian-3.html#doubling-dbl-2001-b
-	delta := new(big.Int).Mul(z, z)
-	delta.Mod(delta, curve.P)
-	gamma := new(big.Int).Mul(y, y)
-	gamma.Mod(gamma, curve.P)
-	alpha := new(big.Int).Sub(x, delta)
-	if alpha.Sign() == -1 {
-		alpha.Add(alpha, curve.P)
-	}
-	alpha2 := new(big.Int).Add(x, delta)
-	alpha.Mul(alpha, alpha2)
-	alpha2.Set(alpha)
-	alpha.Lsh(alpha, 1)
-	alpha.Add(alpha, alpha2)
+// ref. https://hyperelliptic.org/EFD/g1p/auto-shortw-jacobian-0.html#doubling-dbl-2009-l
+func (curve *CurveParams) doubleJacobian(x1, y1, z1 *big.Int) (x3, y3, z3 *big.Int) {
+	x3, y3, z3 = new(big.Int), new(big.Int), new(big.Int)
 
-	beta := alpha2.Mul(x, gamma)
-
-	x3 := new(big.Int).Mul(alpha, alpha)
-	beta8 := new(big.Int).Lsh(beta, 3)
-	beta8.Mod(beta8, curve.P)
-	x3.Sub(x3, beta8)
-	if x3.Sign() == -1 {
-		x3.Add(x3, curve.P)
+	// (x1, y1) is point at infinity in affine coordinates
+	if y1.Sign() == 0 || z1.Sign() == 0 {
+		return
 	}
+
+	// A = X1^2
+	a := new(big.Int).Mul(x1, x1) // 1S
+
+	// B = Y1^2
+	b := new(big.Int).Mul(y1, y1) // 2S
+
+	// C = B^2
+	c := new(big.Int).Mul(b, b) // 3S
+
+	// D = 2*((X1 + B)^2 - A - C)
+	d := new(big.Int).Add(x1, b) // 1add
+	d.Mul(d, d)                  // 4S
+	d.Sub(d, a)                  // 2add
+	d.Sub(d, c)                  // 3add
+	d.Lsh(d, 1)                  // 1*2
+
+	// E = 3*A
+	e := new(big.Int).Lsh(a, 1) // 2*2
+	e.Add(e, a)                 // 4add
+
+	// F = E^2
+	f := new(big.Int).Mul(e, e) // 5S
+
+	// tmp1 = 2*D
+	tmp1 := new(big.Int).Lsh(d, 1) // 3*2
+
+	// tmp2 = 8*C
+	tmp2 := new(big.Int).Lsh(c, 3) // 1*8
+
+	// X3 = F - 2*D
+	x3.Sub(f, tmp1) // 5add
 	x3.Mod(x3, curve.P)
 
-	z3 := new(big.Int).Add(y, z)
-	z3.Mul(z3, z3)
-	z3.Sub(z3, gamma)
-	if z3.Sign() == -1 {
-		z3.Add(z3, curve.P)
-	}
-	z3.Sub(z3, delta)
-	if z3.Sign() == -1 {
-		z3.Add(z3, curve.P)
-	}
-	z3.Mod(z3, curve.P)
-
-	beta.Lsh(beta, 2)
-	beta.Sub(beta, x3)
-	if beta.Sign() == -1 {
-		beta.Add(beta, curve.P)
-	}
-	y3 := alpha.Mul(alpha, beta)
-
-	gamma.Mul(gamma, gamma)
-	gamma.Lsh(gamma, 3)
-	gamma.Mod(gamma, curve.P)
-
-	y3.Sub(y3, gamma)
-	if y3.Sign() == -1 {
-		y3.Add(y3, curve.P)
-	}
+	// Y3 = E * (D - X3) - 8*C
+	y3.Sub(d, x3)    // 6add
+	y3.Mul(e, y3)    // 1M
+	y3.Sub(y3, tmp2) // 7add
 	y3.Mod(y3, curve.P)
 
-	return x3, y3, z3
+	// Z3 = 2 * Y1 * Z1
+	z3.Mul(y1, z1) // 2M
+	z3.Lsh(z3, 1)  // 4*2
+	z3.Mod(z3, curve.P)
+
+	// cost: 2M + 5S + 7add + 4*2 + 1*8
+
+	return
 }
 
-func (curve *CurveParams) ScalarMult(Bx, By *big.Int, k []byte) (*big.Int, *big.Int) {
-	Bz := new(big.Int).SetInt64(1)
+func (curve *CurveParams) ScalarMult(x1, y1 *big.Int, k []byte) (x, y *big.Int) {
+	z1 := new(big.Int).SetInt64(1)
 	x, y, z := new(big.Int), new(big.Int), new(big.Int)
 
 	for _, byte := range k {
 		for bitNum := 0; bitNum < 8; bitNum++ {
 			x, y, z = curve.doubleJacobian(x, y, z)
 			if byte&0x80 == 0x80 {
-				x, y, z = curve.addJacobian(Bx, By, Bz, x, y, z)
+				x, y, z = curve.addJacobian(x1, y1, z1, x, y, z)
 			}
 			byte <<= 1
 		}
@@ -266,8 +286,40 @@ func (curve *CurveParams) ScalarMult(Bx, By *big.Int, k []byte) (*big.Int, *big.
 	return curve.affineFromJacobian(x, y, z)
 }
 
-func (curve *CurveParams) ScalarBaseMult(k []byte) (*big.Int, *big.Int) {
+func (curve *CurveParams) ScalarBaseMult(k []byte) (x, y *big.Int) {
 	return curve.ScalarMult(curve.Gx, curve.Gy, k)
+}
+
+const (
+	p       = "fffffffffffffffffffffffffffffffffffffffffffffffffffffffefffffc2f"
+	n       = "fffffffffffffffffffffffffffffffebaaedce6af48a03bbfd25e8cd0364141"
+	b       = "0000000000000000000000000000000000000000000000000000000000000007"
+	gx      = "79be667ef9dcbbac55a06295ce870b07029bfcdb2dce28d959f2815b16f81798"
+	gy      = "483ada7726a3c4655da4fbfc0e1108a8fd17b448a68554199c47d08ffb10d4b8"
+	bitSize = 256
+)
+
+var (
+	initonce  sync.Once
+	secp256k1 *CurveParams
+)
+
+func initS256() {
+	secp256k1 = &CurveParams{Name: "secp256k1"}
+	secp256k1.P = hexToBigInt(p)
+	secp256k1.N = hexToBigInt(n)
+	secp256k1.B = hexToBigInt(b)
+	secp256k1.Gx = hexToBigInt(gx)
+	secp256k1.Gy = hexToBigInt(gy)
+	secp256k1.BitSize = bitSize
+}
+
+func hexToBigInt(s string) *big.Int {
+	i, ok := new(big.Int).SetString(s, 16)
+	if !ok {
+		panic("invalid hex : " + s)
+	}
+	return i
 }
 
 var mask = []byte{0xff, 0x1, 0x3, 0x7, 0xf, 0x1f, 0x3f, 0x7f}
@@ -339,59 +391,7 @@ func Unmarshal(curve Curve, data []byte) (x, y *big.Int) {
 	return
 }
 
-var initonce sync.Once
-var p384 *CurveParams
-var p521 *CurveParams
-
-func initAll() {
-	initP224()
-	initP256()
-	initP384()
-	initP521()
-}
-
-func initP384() {
-	// See FIPS 186-3, section D.2.4
-	p384 = &CurveParams{Name: "P-384"}
-	p384.P, _ = new(big.Int).SetString("39402006196394479212279040100143613805079739270465446667948293404245721771496870329047266088258938001861606973112319", 10)
-	p384.N, _ = new(big.Int).SetString("39402006196394479212279040100143613805079739270465446667946905279627659399113263569398956308152294913554433653942643", 10)
-	p384.B, _ = new(big.Int).SetString("b3312fa7e23ee7e4988e056be3f82d19181d9c6efe8141120314088f5013875ac656398d8a2ed19d2a85c8edd3ec2aef", 16)
-	p384.Gx, _ = new(big.Int).SetString("aa87ca22be8b05378eb1c71ef320ad746e1d3b628ba79b9859f741e082542a385502f25dbf55296c3a545e3872760ab7", 16)
-	p384.Gy, _ = new(big.Int).SetString("3617de4a96262c6f5d9e98bf9292dc29f8f41dbd289a147ce9da3113b5f0b8c00a60b1ce1d7e819d7a431d7c90ea0e5f", 16)
-	p384.BitSize = 384
-}
-
-func initP521() {
-	// See FIPS 186-3, section D.2.5
-	p521 = &CurveParams{Name: "P-521"}
-	p521.P, _ = new(big.Int).SetString("6864797660130609714981900799081393217269435300143305409394463459185543183397656052122559640661454554977296311391480858037121987999716643812574028291115057151", 10)
-	p521.N, _ = new(big.Int).SetString("6864797660130609714981900799081393217269435300143305409394463459185543183397655394245057746333217197532963996371363321113864768612440380340372808892707005449", 10)
-	p521.B, _ = new(big.Int).SetString("051953eb9618e1c9a1f929a21a0b68540eea2da725b99b315f3b8b489918ef109e156193951ec7e937b1652c0bd3bb1bf073573df883d2c34f1ef451fd46b503f00", 16)
-	p521.Gx, _ = new(big.Int).SetString("c6858e06b70404e9cd9e3ecb662395b4429c648139053fb521f828af606b4d3dbaa14b5e77efe75928fe1dc127a2ffa8de3348b3c1856a429bf97e7e31c2e5bd66", 16)
-	p521.Gy, _ = new(big.Int).SetString("11839296a789a3bc0045c8a5fb42c7d1bd998f54449579b446817afbd17273e662c97ee72995ef42640c550b9013fad0761353c7086a272c24088be94769fd16650", 16)
-	p521.BitSize = 521
-}
-
-// P256 returns a Curve which implements P-256 (see FIPS 186-3, section D.2.3)
-//
-// The cryptographic operations are implemented using constant-time algorithms.
-func P256() Curve {
-	initonce.Do(initAll)
-	return p256
-}
-
-// P384 returns a Curve which implements P-384 (see FIPS 186-3, section D.2.4)
-//
-// The cryptographic operations do not use constant-time algorithms.
-func P384() Curve {
-	initonce.Do(initAll)
-	return p384
-}
-
-// P521 returns a Curve which implements P-521 (see FIPS 186-3, section D.2.5)
-//
-// The cryptographic operations do not use constant-time algorithms.
-func P521() Curve {
-	initonce.Do(initAll)
-	return p521
+func S256() Curve {
+	initonce.Do(initS256)
+	return secp256k1
 }
